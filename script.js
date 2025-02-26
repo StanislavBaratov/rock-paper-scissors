05236be768bdf11506098b738930782adee33170
StanislavBaratov <stanislavbaratov@mail.ru>
2025-02-26 23:02:04 +0500

    Add computer choice logic

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello, world!");
+function getComputerChoice() {
+    const choice = Math.random() * 3;
+    if (choice <= 1) {
+        return "rock";
+    } else if (choice <= 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
